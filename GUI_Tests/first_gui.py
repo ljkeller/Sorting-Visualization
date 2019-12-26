80d6bb546db5ea167494cb813d37fe09c7bceffa
ljkeller <kellerjlucas@gmail.com>
2019-12-26 17:41:27 -0600

    Added sift down visualization for heapsort, resulting in the initial heapify and frequent sift_down calls to be visualized. Changed the visualization process for more sorting methods to show every single swap made internally. The visualizations are much longer now, but 100% accurate of array changes! I'm calling the project finished for now. Might document better.

--- GUI_Tests/first_gui.py
+++ GUI_Tests/first_gui.py
@@ -48,24 +48,72 @@ def merge_sort(graph, boxes, xs):
                     xs[p + 1: q + 1] = xs[p:q]
                     xs[p] = tmp
                     p, mid, q = p + 1, mid + 1, q + 1
-            draw_boxes(graph, boxes, xs)
-            window.read(30)
-
+                    draw_boxes_and_read(graph, boxes, xs, 30)
         unit *= 2
 
+def sift_down(graph, rectangles, arr: list, node: int, n: int) -> None:
+    """
+    A log(h) time function to be used on an array node to heapify a subtree.
+    Works bottom-up, as in parents smaller than children will swap,
+    and iteratively continues.
+    :param arr: Array to be sifted through
+    :param node: Current parent node index of interest
+    :param n: Size of array/tree
+    """
+
+    index = node
+    if index >= n or index*2 + 1 >= n:
+        return
+
+    has_left_child = True
+    left_child = arr[index * 2 + 1]
+    has_right_child = True if index * 2 + 2 < n else False
+    if has_right_child:
+        right_child = arr[index * 2 + 2]
+    else:
+        right_child = None
+
+    while has_left_child and arr[index] < arr[index * 2 + 1] or (
+            has_right_child and arr[index] < arr[index * 2 + 2]):
+        if has_left_child and has_right_child:
+            max_child_index = 2 * index + 1 if left_child >= right_child \
+                else 2 * index + 2
+        else:
+            max_child_index = 2 * index + 1
+        arr[max_child_index], arr[index] = arr[index], arr[max_child_index]
+        index = max_child_index
+        draw_boxes_and_read(graph, rectangles, arr, 30)
+
+        if index*2 + 1 >= n:
+            return
+        else:
+            left_child = arr[index*2 + 1]
+            has_right_child = True if index * 2 + 2 < n else False
+            if has_right_child:
+                right_child = arr[index * 2 + 2]
+
+def heapify(graph, boxes, arr: list) -> None:
+    """
+    A linear time, in place function for creating a max-heap
+    :param arr: Array to be turned into a max-heap
+    :param graph: Graph object we are displaying to
+    :param boxes: Previous collection of graphed elements
+    """
+
+    for i in range(NUM_ELEMENTS - 1, -1, -1):
+        sift_down(graph, boxes, arr, i, NUM_ELEMENTS)
 
 def quick_sort(graph, boxes, arr, left, right):
     if left >= right:
         return
 
-    p = partition(arr, left, right)
-    draw_boxes(graph, boxes, arr)
-    window.read(100)
+    p = partition(graph, boxes, arr, left, right)
+    draw_boxes_and_read(graph, boxes, arr, 100)
     quick_sort(graph, boxes, arr, left, p - 1)
     quick_sort(graph, boxes, arr, p + 1, right)
 
 
-def partition(arr, left, right):
+def partition(graph, boxes, arr, left, right):
     pivot = arr[left]
     i, j = left + 1, right
     while True:  # consider even or odd length lists
@@ -75,9 +123,11 @@ def partition(arr, left, right):
             j -= 1
         if i <= j:
             arr[j], arr[i] = arr[i], arr[j]
+            draw_boxes_and_read(graph, boxes, arr, 30)
         else:
             break
     arr[left], arr[j] = arr[j], arr[left]  # swap pivot in
+    draw_boxes_and_read(graph, boxes, arr, 30)
     return j
 
 
@@ -92,6 +142,9 @@ def draw_boxes(graph, rectangles, elements):
                                               fill_color='black',
                                               line_color='white'))
 
+def draw_boxes_and_read(graph, rectangles, elements, timeout):
+    draw_boxes(graph, rectangles, elements)
+    window.read(timeout)
 
 sg.ChangeLookAndFeel('DarkAmber')
 layout = [
@@ -126,10 +179,10 @@ while True:
                     generated[j], generated[j + 1] = generated[j + 1], \
                                                      generated[j]
                     swap = True
+                    draw_boxes_and_read(graph, boxes, generated, 3)
             if not swap:
                 break
-            draw_boxes(graph, boxes, generated)
-            window.read(30)  # updates the window
+            draw_boxes_and_read(graph, boxes, generated, 30)
     elif event == 'Clear':
         COUNTER = 0
         clear_graph(graph, boxes)
@@ -139,10 +192,10 @@ while True:
             j = i - 1
             while j >= 0 and insert < generated[j]:  # right to left, stable
                 generated[j + 1] = generated[j]
+                draw_boxes_and_read(graph, boxes, generated, 30)
                 j -= 1
             generated[j + 1] = insert
-            draw_boxes(graph, boxes, generated)
-            window.read(30)
+            draw_boxes_and_read(graph, boxes, generated, 30)
     elif event == 'Selection Sort':
         for i in range(len(generated)):
             min_index = i
@@ -151,20 +204,15 @@ while True:
                     min_index = j
             generated[min_index], generated[i] = generated[i], \
                                                  generated[min_index]
-            draw_boxes(graph, boxes, generated)
-            window.read(30)
+            draw_boxes_and_read(graph, boxes, generated, 30)
     elif event == 'Merge Sort':
         merge_sort(graph, boxes, generated)
 
     elif event == 'Heap Sort':
-        for i in range(NUM_ELEMENTS):
-            generated[0], generated[NUM_ELEMENTS - 1 - i] = generated[
-                NUM_ELEMENTS - 1 - i], generated[0]
-            temp = generated[:NUM_ELEMENTS - 1 - i]
-            heapq._heapify_max(temp)
-            generated[:NUM_ELEMENTS - 1 - i] = temp
-            draw_boxes(graph, boxes, generated)
-            window.read(90)
+        heapify(graph, boxes, generated)
+        for i in range(NUM_ELEMENTS-1, 0, -1):
+            generated[0], generated[i] = generated[i], generated[0]
+            sift_down(graph, boxes, generated, 0, i)
 
     elif event == 'Radix Sort':
         num_digits = math.ceil(math.log(CANVAS_HEIGHT, 10))
@@ -174,13 +222,11 @@ while True:
                 remainder = int((generated[i] // math.pow(10, digit))) % 10
                 count_sort[remainder].append(generated[i])
                 flat = [leaves for tree in count_sort for leaves in tree]
-                draw_boxes(graph, boxes, flat + generated[i:])
-                window.read(30)
+                draw_boxes_and_read(graph, boxes, flat + generated[i:], 30)
             generated[:] = [leaves for tree in count_sort for leaves in tree]
             for digit_sublist in count_sort:
                 digit_sublist.clear()
-            draw_boxes(graph, boxes, generated)
-            window.read(100)
+            draw_boxes_and_read(graph, boxes, generated, 100)
 
     elif event == 'Quick Sort':
         quick_sort(graph, boxes, generated, 0, NUM_ELEMENTS - 1)
--- Lect2/Sorting.py
+++ Lect2/Sorting.py
@@ -1,19 +1,21 @@
+import heapq
 import math
 import random
-import heapq
+
 
 def bubble_sort(arr):
     swap = True
     count = 0
-    for i in range(len(arr) - 1): # only to n-2 because inner loop will
+    for i in range(len(arr) - 1):  # only to n-2 because inner loop will
         # cause last element to be largest after first pass
         swap = False;
-        for j in range(len(arr) - i - 1): # Wont check already updated elements
+        for j in range(
+                len(arr) - i - 1):  # Wont check already updated elements
             count += 1
-            if arr[j] > arr[j+1]:
+            if arr[j] > arr[j + 1]:
                 temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
                 swap = True
         if not swap:
             print(count)
@@ -22,35 +24,86 @@ def bubble_sort(arr):
     print(count)
     return
 
+
 def insertion_sort(arr: list):
     for j in range(1, len(arr)):
         insert = arr[j]
         i = j - 1
-        while i >= 0 and insert < arr[i]: #stable, right to left
-            arr[i+1] = arr[i]
-            i-=1
-        arr[i+1] = insert
+        while i >= 0 and insert < arr[i]:  # stable, right to left
+            arr[i + 1] = arr[i]
+            i -= 1
+        arr[i + 1] = insert
+
 
 def quick_sort(arr, left, right):
     if left >= right:
         return
 
     p = partition(arr, left, right)
-    quick_sort(arr, left, p-1)
-    quick_sort(arr, p+1, right)
+    quick_sort(arr, left, p - 1)
+    quick_sort(arr, p + 1, right)
+
 
 def heap_sort(arr):
-    for i in range(len(arr)):
-        arr[0], arr[len(arr) - 1 - i] = arr[len(arr) - 1 - i], arr[0]
-        temp = arr[:len(arr) - 1 - i]
-        heapq._heapify_max(temp)
-        arr[:len(arr) - 1 - i] = temp
+    n = len(arr)
+    heapify(arr)
+    for i in range(len(arr)-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        sift_down(arr, 0, i)
 
+def heapify(arr: list) -> None:  # In place, linear time max-heap function
+    """
+    A linear time, in place function for creating a max-heap
+    :param arr: Array to be turned into a max-heap
+    """
+    for i in range(len(arr) - 1, -1, -1):
+        sift_down(arr, i, len(arr))
+
+
+def sift_down(arr: list, node: int, n) -> None:
+    """
+    A log(h) time function to be used on an array node to heapify a subtree.
+    Works bottom-up, as in parents smaller than children will swap,
+    and iteratively continues.
+    :param arr: Array to be sifted through
+    :param node: Current parent node index of interest
+    :param n: Size of array/tree
+    """
+
+    index = node
+    if index >= n or index*2 + 1 >= n :
+        return
+
+    has_left_child = True
+    left_child = arr[index * 2 + 1]
+    has_right_child = True if index * 2 + 2 < n else False
+    if has_right_child:
+        right_child = arr[index * 2 + 2]
+    else:
+        right_child = None
+
+    while has_left_child and arr[index] < arr[index * 2 + 1] or (
+            has_right_child and arr[index] < arr[index * 2 + 2]):
+        if has_left_child and has_right_child:
+            max_child_index = 2 * index + 1 if left_child >= right_child \
+                else 2 * index + 2
+        else:
+            max_child_index = 2 * index + 1
+        arr[max_child_index], arr[index] = arr[index], arr[max_child_index]
+        index = max_child_index
+
+        if index*2 + 1 >= n:
+            return
+        else:
+            left_child = arr[index*2 + 1]
+            has_right_child = True if index * 2 + 2 < n else False
+            if has_right_child:
+                right_child = arr[index * 2 + 2]
 
 def partition(arr, left, right):
     pivot = arr[left]
     i, j = left + 1, right
-    while True: # consider even or odd length lists
+    while True:  # consider even or odd length lists
         while i <= j and arr[i] <= pivot:
             i += 1
         while i <= j and arr[j] >= pivot:
@@ -59,8 +112,9 @@ def partition(arr, left, right):
             arr[j], arr[i] = arr[i], arr[j]
         else:
             break
-    arr[left], arr[j] = arr[j], arr[left] # swap pivot in
+    arr[left], arr[j] = arr[j], arr[left]  # swap pivot in
     return j
+
 
 def digit_ordering(arr, max):
     num_digits = math.ceil(math.log(max, 10))
@@ -75,25 +129,30 @@ def digit_ordering(arr, max):
     test = [elem for sublist in digits_organized for elem in sublist]
     return digits_organized
 
+
 if __name__ == '__main__':
     arr = [3, 11, 15, 2, 12, 9]
     arr2 = [3, 5, 9, 12, 20, 46]
     rand_arr = [random.randint(0, 820) for i in range(175)]
     rand_arr2 = [random.randint(0, 820) for i in range(175)]
 
-    #print("The array contents are: %s", arr)
+    heap_sort(arr)
+    print(arr)
 
-    x = digit_ordering(rand_arr, 820)
-    #print(x)
-
-
-    quick_sort(arr, 0, len(arr) - 1)
-    #print("The array contents are: %s", arr)
-    print(rand_arr2)
-    heapq._heapify_max(rand_arr2)
-    print(rand_arr2.pop())
-    print(rand_arr2)
-    heap_sort(rand_arr2)
-    print("Also, the array: ", rand_arr2)
-
-    insertion_sort(arr)
+    heap_sort(rand_arr)
+    print(rand_arr)
+    # # print("The array contents are: %s", arr)
+    #
+    # x = digit_ordering(rand_arr, 820)
+    # # print(x)
+    #
+    # quick_sort(arr, 0, len(arr) - 1)
+    # # print("The array contents are: %s", arr)
+    # print(rand_arr2)
+    # heapq._heapify_max(rand_arr2)
+    # print(rand_arr2.pop())
+    # print(rand_arr2)
+    # heap_sort(rand_arr2)
+    # print("Also, the array: ", rand_arr2)
+    #
+    # insertion_sort(arr)
